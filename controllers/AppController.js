fe63f16a3ec6bc144c0df08a3292c4a4eeb0c952
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2024-12-10 17:04:49 +0000

    update server.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,11 +1,9 @@
 // controllers/AppController.js
-
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
-    // Retourne l'état de Redis et MongoDB
     res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
@@ -13,19 +11,10 @@ class AppController {
   }
 
   static async getStats(req, res) {
-    try {
-      // Compte le nombre d'utilisateurs et de fichiers dans la base de données
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
 
-      res.status(200).json({
-        users,
-        files,
-      });
-    } catch (error) {
-      console.error('Error fetching stats:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+    res.status(200).json({ users, files });
   }
 }
 
