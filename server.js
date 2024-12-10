f0967ee415c956db074fbc348a12d8c7b0679804
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2024-12-10 17:59:57 +0000

    update server.js

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ import routes from './routes/index';
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Utiliser les routes définies dans routes/index.js
+// Middleware pour parser les données JSON
+app.use(express.json());
+
+// Charger les routes
 app.use('/', routes);
 
 // Démarrer le serveur
